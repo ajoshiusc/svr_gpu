56f0cf0e0ff6c82e43030d6189668fea5259342f
Anand A. Joshi <ajoshi@usc.edu>
2025-10-14 13:11:43 -0700

    chore: set default --max-series=4; doc updates; set package version to 0.1.0

--- __init__.py
+++ __init__.py
@@ -2,4 +2,7 @@
 
 from .svr_cli import main, run_svr, preprocess_inputs
 
-__all__ = ["main", "run_svr", "preprocess_inputs"]
+# Package version
+__version__ = "0.1.0"
+
+__all__ = ["main", "run_svr", "preprocess_inputs", "__version__"]
