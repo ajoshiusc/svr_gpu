9f447ff072b9cec16def49af3dfbb5e2b304a868
Anand A. Joshi <ajoshi@usc.edu>
2025-10-27 13:27:36 -0700

    feat(simulation): update stack generation scripts to include nonlinear transformations and enhance output structure

--- simstack_scripts/main_simstacks_abdomen.py
+++ simstack_scripts/main_simstacks_abdomen.py
@@ -3,9 +3,9 @@ from simulate_stacks_from_mri_monai_deform import generate_simulated_stacks
 
 if __name__ == "__main__":
 	generate_simulated_stacks(
-		mri_path="/home/ajoshi/Downloads/test_abdomen/39/t1outphase.nii.gz",
-		out_dir="/home/ajoshi/Downloads/test_abdomen/39/sim_stacks/",
-		n_stacks=5,
+		mri_path="/home/ajoshi/Projects/svr_gpu/test_data/39/t1outphase.nii.gz",
+		out_dir="/home/ajoshi/Projects/svr_gpu/test_data/39/sim_stacks_nonlin/",
+		n_stacks=6,
 		slices_per_stack=60,
 		noise_std=0.01,
 		max_disp=5.0,
@@ -14,6 +14,7 @@ if __name__ == "__main__":
 		max_trans_mm=1.0,
 		mb_factor=1,
 		inplane_res=2,
-		slice_thickness=9
+		slice_thickness=9,
+		orientations=[(0, 'x'), (90, 'y'), (180, 'x'), (270, 'y'), (45, 'z'), (135, 'z')]
 	)
 
